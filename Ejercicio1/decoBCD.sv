5dd9ea4c95662cff603cbcd790fba2a53c92161c
Dennis Jimenez <alejimenezc@hotmail.com>
2023-08-20 13:09:53 -0600

    Configuracion e inicio

---
+++ Ejercicio1/decoBCD.sv
@@ -0,0 +1,9 @@
+module decoBCD(A,B,C,D,a,b,c,d,e,f,g,h)
+
+			input A,B,C,D
+			output a,b,c,d,e,f,g,h
+						always @(A,B,C,D)
+				begin
+				if(A==1'b0 & B==1'b0 & C==1'b0 & D==1'b0) a==1'b0
+
+endmodule
