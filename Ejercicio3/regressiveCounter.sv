29aeaca5b8bc5a7935c1911780534b993d32dbc8
Erick Barrantes <erick.cr12@outlook.com>
2023-08-21 16:35:01 -0600

    Created 2bit, 4bit and 6bit testbench.

--- Ejercicio3/regressiveCounter.sv
+++ Ejercicio3/regressiveCounter.sv
@@ -1,18 +1,16 @@
 module regressiveCounter #(parameter N = 10)
 	(input reg [N-1:0] in, input logic decrement, input logic reset,
 	 output reg [N-1:0] out = in);
-
-	reg [N-1:0] count = 1;
+	
+	reg [N-1:0] count = 0;
 	always_ff @(posedge decrement or posedge reset) begin
 		if (reset)	begin
 			out <= in;
 			count <= 0;
 		end
       else begin
-			if (out !== 0) begin
-				out <= in - count;
-				count <= count + 1;
-			end
+			out <= in - count;
+			count <= count + 1;
 		end
 	end
 	
