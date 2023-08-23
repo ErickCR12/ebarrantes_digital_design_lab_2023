47d4c51f865d14c56b48503794840993e04c14ad
Dennis Jimenez <alejimenezc@hotmail.com>
2023-08-22 19:00:40 -0600

    Ejercicio 1 completo

--- Ejercicio1/sevenseg.sv
+++ Ejercicio1/sevenseg.sv
@@ -4,7 +4,7 @@ module sevenseg(input reg[3:0]sw, output reg[8:0]sev);
 				begin
 					if(sw==4'b0000) sev=9'b110000001;
 					else if (sw==4'b0001) sev=9'b111001111;
-					else if (sw==4'b0010) sev=9'b110010011;
+					else if (sw==4'b0010) sev=9'b110010010;
 					else if (sw==4'b0011) sev=9'b110000110;
 					else if (sw==4'b0100) sev=9'b111001100;
 					else if (sw==4'b0101) sev=9'b110100100;
@@ -14,7 +14,7 @@ module sevenseg(input reg[3:0]sw, output reg[8:0]sev);
 					else if (sw==4'b1001) sev=9'b110001100;
 					else if (sw==4'b1010) sev=9'b000000001;
 					else if (sw==4'b1011) sev=9'b001001111;
-					else if (sw==4'b1100) sev=9'b000010011;
+					else if (sw==4'b1100) sev=9'b000010010;
 					else if (sw==4'b1101) sev=9'b000000110;
 					else if (sw==4'b1110) sev=9'b001001100;
 					else if (sw==4'b1111) sev=9'b000100100;
