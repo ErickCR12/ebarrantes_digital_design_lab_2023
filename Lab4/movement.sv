e4a188db9e460d62f3e917009ba38740e837e615
Erick Barrantes <erick.cr12@outlook.com>
2023-10-01 00:41:18 -0600

    Arreglo de bugs en movimiento para arriba y abajo. Módulo ahora recibe clk y rst. Se modifica tb para actuar con clk. Se agrega movimiento diagonal.

--- Lab4/movement.sv
+++ Lab4/movement.sv
@@ -1,22 +1,25 @@
-module movement(input reg [2:0]i_actual,
-					 input reg [2:0]j_actual, 
-					 input logic isUp, 
-					 input logic isRight, 
-					 input logic isLeft, 
-					 input logic isDown,
-					 output reg [2:0]i_next, 
-					 output reg [2:0]j_next);
-					 
-		always @(i_actual, j_actual,isUp, isRight, isLeft, isDown)
-				begin
-					if(i_actual==3'b111 && isUp==1) i_next = i_actual;
-					else if(i_actual==3'b000 && isDown==1) i_next = i_actual;
-					else if(j_actual==3'b111 && isRight==1) j_next = j_actual;
-					else if(j_actual==3'b000 && isLeft==1) j_next = j_actual;
-					else if(isUp==1) i_next=i_actual + 1;
-					else if(isDown==1) i_next=i_actual - 1;
-					else if(isRight==1) j_next=j_actual + 1;
-					else if(isLeft==1) j_next=j_actual - 1;
-				end 
+module movement(
+	input reg [2:0]i_actual, j_actual, 
+	input logic isUp, isRight, isLeft, isDown, clk, rst,
+	output reg [2:0]i_next, j_next
+);
+				
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			i_next = 0;
+			j_next = 0;
+		end else begin
+			if(i_actual==3'b111 && isDown==1) i_next = i_actual;
+			else if(i_actual==3'b000 && isUp==1) i_next = i_actual;
+			else if(j_actual==3'b111 && isRight==1) j_next = j_actual;
+			else if(j_actual==3'b000 && isLeft==1) j_next = j_actual;
+			else begin
+				if(isUp) i_next=i_actual - 1;
+				if(isDown) i_next=i_actual + 1;
+				if(isRight) j_next=j_actual + 1;
+				if(isLeft) j_next=j_actual - 1;
+			end
+		end
+	end 
 					 
 endmodule
--- Lab4/movement_tb.sv
+++ Lab4/movement_tb.sv
@@ -1,57 +1,67 @@
 module movement_tb();
-	reg [2:0] iactual;
-	reg [2:0] jactual;
-	reg [2:0] inext;
-	reg [2:0] jnext;
-	logic up;
-	logic down;
-	logic left;
-	logic right;
+
+	reg [2:0] iactual, jactual, inext, jnext;
+	logic up, down, left, right, clk, rst;
 	
-	movement move(iactual, jactual, up, right, left, down, inext, jnext);
+	always begin
+		#5 clk = 0;
+		#5 clk = 1;
+	end
+	
+	movement move(iactual, jactual, up, right, left, down, clk, rst, inext, jnext);
 	
 	initial begin
-	jactual = 3'b001;
-	iactual = 3'b010;
-	up = 1;
-	down = 0;
-	left = 0;
-	right = 0;
-	#40
-	jactual = 3'b001;
-	iactual = 3'b010;
-	up = 0;
-	down = 1;
-	left = 0;
-	right = 0;
-	#40
-	jactual = 3'b001;
-	iactual = 3'b010;
-	up = 0;
-	down = 0;
-	left = 1;
-	right = 0;
-	#40
-	jactual = 3'b001;
-	iactual = 3'b010;
-	up = 0;
-	down = 0;
-	left = 0;
-	right = 1;
-	#40
-	jactual = 3'b000;
-	iactual = 3'b000;
-	up = 0;
-	down = 1;
-	left = 1;
-	right = 0;
-	#40
-	jactual = 3'b111;
-	iactual = 3'b111;
-	up = 1;
-	down = 0;
-	left = 0;
-	right = 1;
-	end
+		rst = 1;
+		iactual = 0;
+		jactual = 0;
+		up = 0;
+		down = 0;
+		left = 0;
+		right = 0;
+		#20
+		
+		rst = 0;	
+		#20
+		
+		right = 1; //=>[0,1]
+		#20
+		
+		right = 0;
+		down = 1; //=>[1,0]
+		#20
+		
+		down = 0;
+		left = 1; //=>[0,0]
+		#20
+		
+		left = 0;
+		up = 1; //=>[0,0]
+		#20
+		
+		up = 0;
+		iactual = 7;
+		jactual = 7;
+		#20
+		
+		left = 1; //=>[7,6]
+		#20
+		
+		left = 0;
+		up = 1; //=>[6,7]
+		#20
+		
+		up = 0;
+		right = 1; //=>[7,7]
+		#20
+		
+		right = 0;
+		down = 1; //=>[7,7]
+		#20
+		
+		down = 0;
+		left = 1;
+		up = 1; //=> [6,6]
+		
+		end
 endmodule
 	
