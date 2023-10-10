3ec2c47f7eee88a2856cbe9dbe54f2db59f9902f
Marco Rivera <marco99eu@gmail.com>
2023-10-09 20:04:51 -0600

    Fixed fitter for the FPGA

--- Lab4/buscaminas.sv
+++ Lab4/buscaminas.sv
@@ -1,7 +1,7 @@
 module buscaminas (
 	input logic iniciar, seleccionarCasilla, esArriba, esDerecha, esIzquierda, esAbajo, marcarBandera, clk, rst,
 	input reg[5:0] cantBombas,
-	output reg[6:0] tablero [7:0][7:0],
+	// output reg[6:0] tablero [7:0][7:0],
 	output reg [2:0] i_actual, j_actual,
 	output logic [3:0] state,
 	output logic vgaclk,
@@ -9,7 +9,8 @@ module buscaminas (
 	output logic sync_b, blank_b,
 	output logic [7:0] r, g, b
 );
-
+	
+	reg[6:0] tablero [7:0][7:0];
 	logic tableroGenerado, esVictoria, movimientoValido, bomba; //inputs para fsm
 	logic enable_matriz, enable_victoria, enable_mov, enable_seleccion, enable_derrota, enable_casillas, enable_bandera; //outputs de fsm
 	reg [2:0] i_next, j_next;
--- Lab4/buscaminas_tb.sv
+++ Lab4/buscaminas_tb.sv
@@ -3,13 +3,13 @@ module buscaminas_tb();
 	logic iniciar, seleccionarCasilla, esArriba, esDerecha, esIzquierda, esAbajo, marcarBandera, clk, rst;
 	logic [3:0] state;
 	reg[5:0] cantBombas;
-	reg[6:0] tablero [7:0][7:0];
+	// reg[6:0] tablero [7:0][7:0];
 	reg [2:0] i_actual, j_actual;
 
 	buscaminas bm (
 		iniciar, seleccionarCasilla, esArriba, esDerecha, esIzquierda, esAbajo, marcarBandera, clk, rst,
 		cantBombas,
-		tablero, i_actual, j_actual, state
+		i_actual, j_actual, state
 	);
 	
 	always begin
@@ -63,10 +63,10 @@ module buscaminas_tb();
 		
 		marcarBandera = 0;
 		seleccionarCasilla = 1;
-		
+		/*
 		for (int i = 0; i < 8; i = i + 1)
 			for (int j = 0; j < 8; j = j + 1) 
-				$display("tablero[%0d][%0d] = %b", i, j, tablero[i][j]);
+				$display("tablero[%0d][%0d] = %b", i, j, tablero[i][j]);*/
 		
 	end	
 endmodule
