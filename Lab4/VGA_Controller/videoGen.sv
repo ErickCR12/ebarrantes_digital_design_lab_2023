5f40bcb1666319d3734cd1e3c8560830c6fe3823
Marco Rivera <marco99eu@gmail.com>
2023-10-09 19:24:54 -0600

    Tablero pintado

--- Lab4/VGA_Controller/videoGen.sv
+++ Lab4/VGA_Controller/videoGen.sv
@@ -4,7 +4,7 @@ module videoGen(
 );
 	logic inrect;
 	
-	parameter size = 10'd60;
+	parameter size = 10'd56;
 	parameter frame = 10'd4;
 	
 	int colIndex, rowIndex, col_px, row_px;
@@ -15,9 +15,9 @@ module videoGen(
 	assign col_px = colIndex*(size+frame);
 	assign row_px = rowIndex*(size+frame);
 	
-	assign inrect = x >= col_px && x < col_px+size && y >= row_px && y < row_px+size;
+	assign inrect = (colIndex < 8 && rowIndex < 8) && (x >= col_px && x < col_px+size && y >= row_px && y < row_px+size);
 	
-	assign {r, g, b} = inrect ? {8'hFF, 8'hFF, 8'hFF} : {8'h00, 8'h00, 8'h00};
+	assign {r, g, b} = inrect ? {8'h00, 8'hFF, 8'h00} : {8'h00, 8'h00, 8'h00};
 
 	
 	//assign inrect = (x >= width + frame & x < width - frame & y >= height + frame & y < height - frame);
